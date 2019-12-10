b848278f04f0349ea005349fc50a6b37d4a42003
Benjamin Small <ben20262@aim.com>
2019-12-10 01:32:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+def madge_maker (name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (array)
+  brray = []
+  array.each do |name|
+    brray << "Hello, my name is #{name}"
+  end
+  brray
+end
+
+def room_assignment (array)
+  brray
+  array.each_index do |index|
+    brray << "Hello, #{array[index]}! You'll be assigned to room #{index}"
+  end
+  nrray
+end
