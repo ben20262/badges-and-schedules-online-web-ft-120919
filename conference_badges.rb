6651ff618c025a7cfb3decb1d9d0df4c6d4c6a4e
Benjamin Small <ben20262@aim.com>
2019-12-10 01:37:22 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,7 @@ def batch_badge_creator (array)
 end
 
 def assign_rooms (array)
-  brray
+  brray = []
   array.each_index do |index|
     brray << "Hello, #{array[index]}! You'll be assigned to room #{index}."
   end
